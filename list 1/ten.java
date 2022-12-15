605d70538e93da9f759728fe8c90403e1e4074f4
Fenil <21bmiit160@gmail.com>
2022-12-15 20:04:46 +0530

    ten complete

---
+++ list 1/ten.java
@@ -0,0 +1,25 @@
+// Write a program to print all possible combinations of three digits. Take care that no
+// combination gets repeated
+
+import java.util.Scanner;
+
+public class ten {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[3];
+        System.out.println("Please Not Down Number Should Be One One Digit");
+        System.out.println("Enter the number: ");
+        for (int i = 0; i < 3; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    if (i != j && j != k && k != i) {
+                        System.out.println(arr[i] + "" + arr[j] + "" + arr[k]);
+                    }
+                }
+            }
+        }
+    }
+}
