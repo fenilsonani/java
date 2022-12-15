b8b8b318b0f3dcc7f3cae13cee0604d3e56f9fb8
Fenil <21bmiit160@gmail.com>
2022-12-15 21:10:59 +0530

    twelve update

--- list 1/twelve.java
+++ list 1/twelve.java
@@ -1,16 +1,21 @@
 // Write a program to find the sum of all integers greater than 100 and less than 200
 // that are divisible by 7
 
+import java.util.Scanner;
+
 public class twelve {
-    
     public static void main(String[] args) {
         int sum = 0;
-        for (int i = 101; i < 200; i++) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Amount Of minimum number ");
+        int min=sc.nextInt();
+        System.out.println("Enter Amount Of maximum number ");
+        int max=sc.nextInt();
+        for (int i = min; i < max; i++) {
             if (i % 7 == 0) {
                 sum += i;
             }
         }
         System.out.println("Sum of all integers greater than 100 and less than 200 that are divisible by 7: " + sum);
     }
-
 }
