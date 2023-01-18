3ffbc505656ab610f8440ba1593a7f8d6a9d926e
Fenil Sonani <21bmiit160@gmail.com>
2023-01-18 22:14:29 +0530

    cutome

---
+++ customer_exeption.java/customer_exeption.java
@@ -0,0 +1,36 @@
+// write a code that will throw an user defined exception simple and explain it
+
+import java.util.Scanner;
+
+class customer_exeption extends Exception
+{
+    public customer_exeption(String s)
+    {
+        super(s);
+    }
+}
+
+class customer_exeption_main 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age");
+        int age = sc.nextInt();
+        try
+        {
+            if(age<18)
+            {
+                throw new customer_exeption("You are not eligible to vote");
+            }
+            else
+            {
+                System.out.println("You are eligible to vote");
+            }
+        }
+        catch(customer_exeption e)
+        {
+            System.out.println(e);
+        }
+    }
+}
---
+++ customer_exeption.java/explai.md
@@ -0,0 +1,14 @@
+<!-- explain custome exeption in simple way -->
+
+# Custom Exception
+
+## What is Custom Exception?
+
+Custom Exception is a class that inherits the Exception class or any of its subclasses. It is used to create a user-defined exception. It is a way to customize the exception according to our need.
+
+## Why Custom Exception?
+
+
+* We can create our own exception class by extending the Exception class or any of its subclasses.
+* We can create our own exception class by extending the RuntimeException class or any of its subclasses.
+* We can create our own exception class by extending the Throwable class or any of its subclasses.
